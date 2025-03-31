55686389b875a8a80b93d8e5a9f5d7d7b45377b8
Rohit Kushwaha <kushrohit795@gamil.com>
2025-03-31 19:16:19 +0530

    all files

---
+++ demo.py
@@ -0,0 +1,19 @@
+# # from src.logger import logging
+
+# # logging.debug("This is a debug message")
+
+
+
+# # --------------------------------------------------------------------------------
+
+# # below code is to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
